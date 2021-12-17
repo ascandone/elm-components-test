43eb8ab95da0ec4c40483bf0562f757be9067252
ascandone <alescandone@gmail.com>
2021-12-17 23:51:15 +0100

    added card component

---
+++ src/Components/Card.elm
@@ -0,0 +1,136 @@
+module Components.Card exposing
+    ( Attribute
+    , Slot
+    , Variant
+    , actions
+    , body
+    , class
+    , dataTestId
+    , media
+    , outline
+    , raised
+    )
+
+import Html exposing (..)
+import Html.Attributes as Attrs
+import Utils
+
+
+type alias ConfigMap a =
+    { body : a
+    , media : a
+    , actions : a
+    }
+
+
+type alias Config msg =
+    { body : List (Html msg)
+    , media : Maybe { src : String }
+    , actions : List (Html msg)
+    , attributesMap : ConfigMap (List (Html.Attribute msg))
+    }
+
+
+defaultConfig : Config msg
+defaultConfig =
+    { body = []
+    , media = Nothing
+    , actions = []
+    , attributesMap = ConfigMap [] [] []
+    }
+
+
+type ConfigCtx
+    = Card
+    | Media
+
+
+type Attribute ctx msg
+    = Attribute (ConfigCtx -> Config msg -> Config msg)
+
+
+type Variant
+    = Raised
+    | Outline
+
+
+dataTestId : String -> Attribute { r | media : (), card : () } msg
+dataTestId _ =
+    Attribute (\_ c -> c)
+
+
+class : String -> Attribute { r | card : () } msg
+class _ =
+    Attribute (\_ c -> c)
+
+
+type Slot msg
+    = Slot (Config msg -> Config msg)
+
+
+media : List (Attribute { r | media : () } msg) -> { src : String } -> Slot msg
+media attrs args =
+    Slot <|
+        \config ->
+            Utils.getMakeConfig
+                { unwrap = \(Attribute f) -> f Media
+                , defaultConfig = { config | media = Just args }
+                }
+                attrs
+
+
+body : List (Html msg) -> Slot msg
+body children =
+    Slot <| \config -> { config | body = children }
+
+
+actions : List (Html msg) -> Slot msg
+actions children =
+    Slot <| \config -> { config | actions = children }
+
+
+raised : List (Attribute { r | card : () } msg) -> List (Slot msg) -> Html msg
+raised =
+    view Raised
+
+
+outline : List (Attribute { r | card : () } msg) -> List (Slot msg) -> Html msg
+outline =
+    view Outline
+
+
+view : Variant -> List (Attribute { r | card : () } msg) -> List (Slot msg) -> Html msg
+view variant attributes slots =
+    let
+        cardAttributes =
+            List.map (\(Attribute f) -> f Card) attributes
+
+        slotsAttributes =
+            slots |> List.map (\(Slot f) -> f)
+
+        config =
+            Utils.getMakeConfig
+                { defaultConfig = defaultConfig
+                , unwrap = identity
+                }
+                (cardAttributes ++ slotsAttributes)
+    in
+    div
+        [ Attrs.class "rounded bg-white"
+        , Attrs.class <|
+            case variant of
+                Raised ->
+                    "border shadow-md hover:shadow-xl transition-all duration-200"
+
+                Outline ->
+                    "border"
+        ]
+        [ case config.media of
+            Nothing ->
+                text ""
+
+            Just { src } ->
+                img [ Attrs.class "rounded-t mb-2", Attrs.src src ] []
+        , div [ Attrs.class "px-4 py-2" ] config.body
+        , div [ Attrs.class "px-4 mt-5 mb-3 flex gap-x-2 justify-end" ] config.actions
+        ]
--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (Model, main)
 import Browser
 import Components.Autocomplete as Autocomplete
 import Components.Button as Button
+import Components.Card as Card
 import Components.Switch as Switch
 import Components.TextField as TextField
 import Components.Toggle as Toggle
@@ -157,14 +158,58 @@ viewSection { children, title, example } =
 
 viewSections : List (Html msg) -> Html msg
 viewSections sections =
-    div [ class "max-w-screen-xl mx-auto my-5 mb-20" ]
+    div [ class "max-w-screen-xl mx-auto py-5 pb-20" ]
         (List.intersperse hRow sections)
+
+
+src : String
+src =
+    "https://mui.com/static/images/cards/contemplative-reptile.jpg"
 
 
 view : Model -> Html Msg
 view model =
     viewSections
         [ viewSection
+            { title = "Card"
+            , example = """Card.raised [ Card.dataTestId "lizard-card" ]
+    [ Card.media [] { src = "..." }
+    , Card.body
+        [ h1 [] [ text "Lizard" ]
+        , p [] [ text "Lizards are a ..." ]
+        ]
+    , Card.actions
+        [ Button.ghost
+            [ Button.size Button.sm
+            , Button.onClick Share
+            ]
+            "Share"
+        , Button.ghost
+            [ Button.size Button.sm
+            , Button.onClick Fav
+            ]
+            "Favorite"
+        ]
+    ]
+            """
+            , children =
+                [ div [ class "max-w-sm" ]
+                    [ Card.raised [ Card.dataTestId "lizard-card" ]
+                        [ Card.media [] { src = src }
+                        , Card.body
+                            [ h2 [ class "font-semibold text-xl" ] [ text "Lizard" ]
+                            , p [ class "text-gray-600" ]
+                                [ text "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica" ]
+                            ]
+                        , Card.actions
+                            [ Button.ghost [ Button.size Button.sm ] "Share"
+                            , Button.ghost [ Button.size Button.sm ] "Favorite"
+                            ]
+                        ]
+                    ]
+                ]
+            }
+        , viewSection
             { title = "Text fields"
             , example = """TextField.view
     [ TextField.value model.textField
@@ -184,7 +229,6 @@ view model =
                     ]
                 , TextField.view
                     [ TextField.value ""
-                    , TextField.autofocus True
                     , TextField.placeholder "example@gmail.com"
                     ]
                 , TextField.view
