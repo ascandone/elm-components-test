cca5883b1fcce9fbdc65ea38c4705ffd79333f3c
Alessandro Scandone <alescandone@gmail.com>
2021-12-18 14:32:07 +0100

    added textfield section

---
+++ src/Section/TextField.elm
@@ -0,0 +1,95 @@
+module Section.TextField exposing
+    ( Model
+    , Msg
+    , get
+    , init
+    , update
+    )
+
+import Components.ActionButton as ActionButton
+import Components.TextField as TextField
+import FeatherIcons
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Section
+
+
+type alias Model =
+    { textField : String
+    }
+
+
+init : Model
+init =
+    { textField = "my-mail@example.com"
+    }
+
+
+type Msg
+    = Input String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input str ->
+            { model | textField = str }
+
+
+validateEmail : String -> Result String ( String, String )
+validateEmail mail =
+    case String.split "@" mail of
+        [ name, domain ] ->
+            Ok ( name, domain )
+
+        _ ->
+            Err "Invalid email"
+
+
+get : { a | textField : String } -> (Msg -> msg) -> Section.Section msg
+get model =
+    Section.make
+        { title = "Text fields"
+        , example = """TextField.view
+    [ TextField.value model.textField
+    , TextField.onInput Input
+    , TextField.validation (validateEmail model.textField)
+    , TextField.autofocus True
+    , TextField.placeholder "example.gmail.com"
+    , TextField.icon Icons.user
+    ]
+"""
+        , children =
+            [ pre [ class "overflow-auto" ] [ text ("model.textField = " ++ model.textField) ]
+            , TextField.view
+                [ TextField.value model.textField
+                , TextField.onInput Input
+                , TextField.validation (validateEmail model.textField)
+                , TextField.placeholder "example@gmail.com"
+                ]
+            , TextField.view
+                [ TextField.value ""
+                , TextField.placeholder "example@gmail.com"
+                ]
+            , TextField.view
+                [ TextField.value ""
+                , TextField.validation (Err "Inserisci una mail valida")
+                , TextField.placeholder "example@gmail.com"
+                ]
+            , TextField.view
+                [ TextField.value ""
+                , TextField.placeholder "example@gmail.com"
+                , TextField.disabled True
+                ]
+            , TextField.view
+                [ TextField.value ""
+                , TextField.placeholder "example@gmail.com"
+                , TextField.icon FeatherIcons.user
+                ]
+            , TextField.view
+                [ TextField.value ""
+                , TextField.placeholder "example@gmail.com"
+                , TextField.actionIcon [ ActionButton.size ActionButton.sm ] FeatherIcons.x
+                ]
+            ]
+        }
