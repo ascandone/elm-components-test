df9ed4be489f77dfacec2c397e6c3e962c2fef77
Alessandro Scandone <alescandone@gmail.com>
2021-12-18 14:25:20 +0100

    add button section

---
+++ src/Section/Button.elm
@@ -0,0 +1,34 @@
+module Section.Button exposing (..)
+
+import Components.Button as Button
+import FeatherIcons
+import Section exposing (Section)
+
+
+buttons : Section msg
+buttons =
+    Section.static
+        { title = "Buttons"
+        , example = """Button.primary [ Button.size Button.lg ] "Click me"
+
+Button.outline
+    [ Button.size Button.md
+    , Button.icon Icons.cross
+    ]
+    "Click me"
+"""
+        , children =
+            [ Button.primary [ Button.size Button.lg ] "Primary lg"
+            , Button.primary [ Button.size Button.md ] "Primary md"
+            , Button.primary [ Button.size Button.sm ] "Primary sm"
+            , Button.outline [ Button.size Button.lg ] "Outline lg"
+            , Button.outline [ Button.size Button.md ] "Outline md"
+            , Button.outline [ Button.size Button.sm ] "Outline sm"
+            , Button.outline
+                [ Button.size Button.lg
+                , Button.icon FeatherIcons.download
+                ]
+                "Icon"
+            , Button.ghost [ Button.size Button.sm ] "Link"
+            ]
+        }
