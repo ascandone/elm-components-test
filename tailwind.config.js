0e2804f2f7cc58b68191c3538a22879f547b514d
Alessandro Scandone <alescandone@gmail.com>
2021-12-19 22:58:16 +0100

    edit fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,elm}"],
    theme: {
+      fontFamily: {
+        sans: ["inter", "sans-serif"],
+        mono: ["Fira Code", "monospace"]
+      },
       extend: {
         boxShadow: {
           soft: "0 4px 42px rgba(0, 0, 0, 0.15)",
@@ -8,7 +12,6 @@ module.exports = {
         transitionTimingFunction: {
             custom: 'cubic-bezier(0.4,0,0.46,1.66)',
         }
-
       }
   },
   plugins: [],
